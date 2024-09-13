175c6760d89be77ccc4c975ff94fbf46d3057fae
Mercedes Egido <mercedes@huridocs.org>
2024-09-13 18:29:31 +0200

    dark mode state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import Header from './components/header/header';
 import styles from './app.module.css';
 import LoginForm from './components/form/login-form';
+import React from 'react';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+
+  const handleDarkMode = () => {
+    !isDarkMode ? setIsDarkMode(true) : setIsDarkMode(false);
+  };
+
   return (
-    <>
-      <Header />
+    <div
+      style={{
+        // NOTE: This is a just-for-fun mini demo, not a
+        // full-featured Dark Mode implementation!
+        '--color-bg': isDarkMode ? '#f8f8e0' : 'white',
+        '--color-text': isDarkMode ? 'black' : 'black',
+      }}
+    >
+      <Header isDarkMode={isDarkMode} handleDarkMode={handleDarkMode} />
       <main className={styles.wrapper}>
         <LoginForm />
       </main>
-    </>
+    </div>
   );
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import styles from './header.module.css';
 
-function Header() {
+function Header({ isDarkMode, handleDarkMode }) {
+  console.log({ isDarkMode });
   return (
     <header className={styles.wrapper}>
       <h1>Some website</h1>
       <div className="actions">
         <div className="actions-item">
-          <button>Activate dark mode</button>
+          <button onClick={handleDarkMode}>
+            {!isDarkMode ? 'Activate dark mode' : 'Deactivate dark mode'}
+          </button>
         </div>
       </div>
     </header>
