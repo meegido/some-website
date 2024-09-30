c157a0e3f1cca608df421db643557ef578216946
Mercedes Egido <mercedes@huridocs.org>
2024-09-30 13:25:38 +0200

    adding html tip calculator

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import styles from './home.module.css';
 
 const Home = () => {
   return (
     <>
-      <h1>Hello to Some website, you are logged in! 🤠</h1>
+      <section className={styles['page__wrapper']}>
+        <h1 className={styles['page__title']}>Split your bill 💸</h1>
+        <div className={styles['calculator__wrapper']}>
+          <section className={styles['calculator__config']}>
+            <article className={`${styles.config}`}>
+              <label>Bill</label>
+              <input type="text" />
+            </article>
+            <article className={styles.config}>
+              <p>Select a tip</p>
+              <div className={styles.tip}>
+                <button className={styles['config__button']}>5%</button>
+                <button className={styles['config__button']}>10%</button>
+                <button className={styles['config__button']}>15%</button>
+                <button className={styles['config__button']}>20%</button>
+                <button className={styles['config__button']}>50%</button>
+                <button className={styles['config__button']}>Custom</button>
+              </div>
+            </article>
+            <article className={styles.config}>
+              <label htmlFor="people">Number of people</label>
+              <input type="text" />
+            </article>
+          </section>
+          <section className={styles.display}>
+            <article className={styles['display__result']}>
+              <div className={styles.legend}>
+                <h3>Tip Amount</h3>
+                <p>/person</p>
+              </div>
+              <div className={styles.amount}>
+                <p>4.5€</p>
+              </div>
+            </article>
+            <article className={styles['display__result']}>
+              <div className={styles.legend}>
+                <h3>Total</h3>
+                <p>/person</p>
+              </div>
+              <div className={styles.amount}>
+                <p>34.5€</p>
+              </div>
+            </article>
+            <div className={styles.reset}>
+              <button className={styles['reset__button']}>Reset</button>
+            </div>
+          </section>
+        </div>
+      </section>
     </>
   );
 };
---
+++ src/components/home.test.jsx
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest';
+import Home from './home';
+import { render } from '@testing-library/react';
+
+describe('Home page', () => {
+  it('should render the home page', () => {
+    render(<Home />);
+
+    expect(document.querySelector('h1')).toHaveTextContent(
+      'Hello to Some website, you are logged in! 🤠'
+    );
+  });
+});
