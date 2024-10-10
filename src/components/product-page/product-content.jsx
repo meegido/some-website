de147dc91d1017d5a4da7186da40661c66acecf5
Mercedes Egido <mercedes@huridocs.org>
2024-10-10 15:52:04 +0200

    showing product price with discount on mount

---
+++ src/components/product-page/product-content.jsx
@@ -0,0 +1,13 @@
+import { PRODUCT_IMAGES } from '../../assets/images/product/product-images';
+
+export const product = {
+  id: 'abc123',
+  brand: 'Sneaker Company',
+  title: 'Fall Limited Edition Sneakers',
+  description: `These low-profile sneakers are your perfect causal wear companion. Featuring a durable
+          rubber outer sole, they&rsquo;ll withstand everything the weather can offer.`,
+  price: '250.00',
+  // price: '125.00',
+  discount: '50',
+  photos: PRODUCT_IMAGES,
+};
--- src/components/product-page/product-page.jsx
+++ src/components/product-page/product-page.jsx
@@ -1,23 +1,29 @@
 import styles from './product-page.module.css';
-import { PRODUCT_IMAGES } from '../../assets/images/product/product-images';
 import React from 'react';
-import ProductDetail from './components/product-detail';
 import ProductGallery from './components/product-gallery';
+import { product } from './product-content';
 
 const ProductPage = () => {
-  const [selectedImageIndex, setSelectedImageIndex] = React.useState(0);
+  const [selectedImageIndex, setSelectedImageIndex] = React.useState(product.price);
+  const [discountPrice, setDiscountPrice] = React.useState(0);
+
+  React.useEffect(() => {
+    product.discount && product.discount !== undefined
+      ? setDiscountPrice((product.price * product.discount) / 100)
+      : setDiscountPrice(product.price);
+  }, []);
 
   const handleNextImage = () => {
     setSelectedImageIndex((prevIndex) => {
-      return (prevIndex + 1) % PRODUCT_IMAGES.length;
-      // return prevIndex === PRODUCT_IMAGES.length - 1 ? 0 : prevIndex + 1;
+      return (prevIndex + 1) % product.photos.length;
+      // return prevIndex === product.photos.length - 1 ? 0 : prevIndex + 1;
     });
   };
 
   const handlePrevImage = () => {
     setSelectedImageIndex((prevIndex) => {
-      return (prevIndex - 1 + PRODUCT_IMAGES.length) % PRODUCT_IMAGES.length;
-      // return prevIndex === 0 ? PRODUCT_IMAGES.length - 1 : prevIndex - 1;
+      return (prevIndex - 1 + product.photos.length) % product.photos.length;
+      // return prevIndex === 0 ? PRODUCT.photos.length - 1 : prevIndex - 1;
     });
   };
 
@@ -25,13 +31,36 @@ const ProductPage = () => {
     <>
       <div className={styles.page__wrapper}>
         <ProductGallery
-          images={PRODUCT_IMAGES}
+          images={product.photos}
           selectedImageIndex={selectedImageIndex}
           setSelectedImageIndex={setSelectedImageIndex}
           handleNextImage={handleNextImage}
           handlePrevImage={handlePrevImage}
         />
-        <ProductDetail />
+        <section className={styles.product__wrapper}>
+          <div className={styles.product__description}>
+            <h3>{product.brand}</h3>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+          </div>
+          <div className={styles.product__price}>
+            <div>
+              <p>${discountPrice}</p>
+              <p>{product.discount}%</p>
+            </div>
+            <p className={styles.final__price}>${product.price}</p>
+          </div>
+          <div className={styles.product__cart}>
+            <div className={styles.product__quantity}>
+              <button>-</button>
+              <p>0</p>
+              <button>+</button>
+            </div>
+            <div className={styles.button__wrapper}>
+              <button className={styles.add__button}>Add to cart</button>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
