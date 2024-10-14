7dc3357cec10722d8805892907b3eb43f4ad56d2
Mercedes Egido <mercedes@huridocs.org>
2024-10-14 18:03:37 +0200

    cart header popover

--- src/shared/components/header/header.jsx
+++ src/shared/components/header/header.jsx
@@ -3,14 +3,16 @@ import styles from './header.module.css';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { ThemeContext } from '../../../providers/theme-provider';
 import { UserContext } from '../../../providers/user-provider';
-import { ChevronDown, ChevronUp, Moon, ShoppingCart, Sun } from 'lucide-react';
+import { Moon, ShoppingCart, Sun, Trash } from 'lucide-react';
 import Dropdown from './dropdown/dropdown';
+import productThumbnail from '../../../assets/images/product/image-product-1.jpg';
 
 const Header = () => {
   const navigate = useNavigate();
   const { theme, toggleTheme } = React.useContext(ThemeContext);
   const { logout, isLoggedIn } = React.useContext(UserContext);
   const [isProfileOpen, setProfileOpen] = React.useState(false);
+  const [isCartOpen, setIsCartOpen] = React.useState(false);
   const dropdownRef = React.useRef();
 
   const handleClickOutside = (event) => {
@@ -44,6 +46,10 @@ const Header = () => {
     setProfileOpen((prevIsOpen) => !prevIsOpen);
   };
 
+  const toggleCart = () => {
+    setIsCartOpen((prevIsOpen) => !prevIsOpen);
+  };
+
   return (
     <div className={styles.header__wrapper} data-theme={theme}>
       <header className={styles.header}>
@@ -63,9 +69,42 @@ const Header = () => {
         <div className={styles.actions}>
           <div className={styles.action__items}>
             {isLoggedIn && (
-              <button className={styles.header__button}>
-                <ShoppingCart />
-              </button>
+              <div className={styles.cart__popover}>
+                <button onClick={toggleCart} className={styles.header__button}>
+                  <ShoppingCart />
+                </button>
+                {isCartOpen && (
+                  <div className={styles.cart__wrapper}>
+                    <div className={styles.popover__title}>
+                      <h3>Cart</h3>
+                    </div>
+                    <section className={styles.cart__content}>
+                      <img
+                        className={styles.cart__thumbnail}
+                        src={productThumbnail}
+                        alt="Image product cart"
+                      />
+                      <div className={styles.product__details}>
+                        <p>Fall Limited Edition Sneakers</p>
+                        <div className={styles.cart__price}>
+                          <p>$125.00</p>
+                          <p>x</p>
+                          <p>3</p>
+                          <p>
+                            <b>$375.00</b>
+                          </p>
+                        </div>
+                      </div>
+                      <div className={styles.remove}>
+                        <Trash />
+                      </div>
+                    </section>
+                    <div className={styles.cart__button}>
+                      <button>Checkout</button>
+                    </div>
+                  </div>
+                )}
+              </div>
             )}
             <button className={styles.header__button} onClick={toggleTheme}>
               {theme === 'light' ? <Sun /> : <Moon />}
