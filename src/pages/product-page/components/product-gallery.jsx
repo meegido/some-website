2e754b0d05f6b64c89e40432c0009a0b36accf3a
Mercedes Egido <mercedes.egido@hey.com>
2024-10-16 16:06:07 +0200

    lightbox style

--- src/pages/product-page/components/product-gallery.jsx
+++ src/pages/product-page/components/product-gallery.jsx
@@ -2,6 +2,7 @@ import styles from './product-gallery.module.css';
 import ThumbnailButton from './thumbnail-button';
 import useIsMobile from '../../../hooks/use-window-resize';
 import React from 'react';
+import { X } from 'lucide-react';
 
 const ProductGallery = ({
   images,
@@ -11,10 +12,21 @@ const ProductGallery = ({
   selectedImageIndex,
 }) => {
   const isMobile = useIsMobile();
+  const [isLightboxOpen, setIsLightboxOpen] = React.useState(false);
+  const [imageToShow, setImageToShow] = React.useState('');
+  const lightboxRef = React.useRef();
+
+  const showImage = (imageUrl) => {
+    setImageToShow(imageUrl);
+    setIsLightboxOpen(true);
+  };
+
+  const dismissLightbox = () => {
+    setIsLightboxOpen(false);
+  };
 
   return (
     <div className={styles.carousel}>
-      <div></div>
       <section className={styles.carousel__wrapper}>
         {images.map((imageUrl, index) => (
           <img
@@ -22,8 +34,31 @@ const ProductGallery = ({
             src={imageUrl}
             alt={`Product image ${index}`}
             style={{ translate: `${-100 * selectedImageIndex}%` }}
+            onClick={() => showImage(imageUrl)}
           />
         ))}
+        {isLightboxOpen ? (
+          <section className={styles.lightbox} ref={lightboxRef}>
+            <div className={styles.lightbox__wrapper}>
+              <div className={styles.ligthbox__close}>
+                <button onClick={dismissLightbox}>
+                  <X />
+                </button>
+              </div>
+              <div className={styles.lightbox__controls}>
+                <button onClick={handlePrevImage}>←</button>
+                <img
+                  className={styles.ligthbox__image}
+                  src={imageToShow}
+                  alt={`Product image lightbox`}
+                />
+                <button onClick={handleNextImage}>→</button>
+              </div>
+            </div>
+          </section>
+        ) : (
+          ''
+        )}
       </section>
       {isMobile ? (
         <section className={styles.carousel__controls}>
