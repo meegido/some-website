1f314ad5324f1a22cd045134697118c8e5a25a8b
Mercedes Egido <mercedes.egido@hey.com>
2024-10-21 13:04:51 +0200

    landing hero desktop

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,12 +1,24 @@
 import LoginForm from './form/login-form';
+import styles from './landing.module.css';
 
 const Landing = () => {
   return (
-    <div>
-      <section>
-        <h1>A place to hone my skills</h1>
+    <div className={styles.landing__wrapper}>
+      <section className={styles.hero}>
+        <h1 className={styles.landing__title}>A place where to hone my skills</h1>
+        <p>
+          See how I approach and solve Frontend coding problems. This site is a working progress to
+          learn React, testing and architecture matters.
+        </p>
       </section>
-      <LoginForm />
+      <section className={styles.form__wrapper}>
+        <aside className={styles.disclaimer}>
+          <p>Login (local storage) to explore.</p>
+        </aside>
+        <article className={styles.form}>
+          <LoginForm />
+        </article>
+      </section>
     </div>
   );
 };
