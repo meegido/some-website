775ff4e86725fee6cb20f6fb0e7346438ca73003
Mercedes Egido <mercedes.egido@hey.com>
2024-10-21 12:12:43 +0200

    vercel routes

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,7 +1,14 @@
 import LoginForm from './form/login-form';
 
 const Landing = () => {
-  return <LoginForm />;
+  return (
+    <div>
+      <section>
+        <h1>A place to hone my skills</h1>
+      </section>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Landing;
