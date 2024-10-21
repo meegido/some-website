c710f13e25a99da21b66678ba822864d82d6a90a
Mercedes Egido <mercedes.egido@hey.com>
2024-10-21 13:25:52 +0200

    wrapping hero

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -4,21 +4,23 @@ import styles from './landing.module.css';
 const Landing = () => {
   return (
     <div className={styles.landing__wrapper}>
-      <section className={styles.hero}>
-        <h1 className={styles.landing__title}>A place where to hone my skills</h1>
-        <p>
-          See how I approach and solve Frontend coding problems. This site is a working progress to
-          learn React, testing and architecture matters.
-        </p>
-      </section>
-      <section className={styles.form__wrapper}>
-        <aside className={styles.disclaimer}>
-          <p>Login (local storage) to explore.</p>
-        </aside>
-        <article className={styles.form}>
-          <LoginForm />
-        </article>
-      </section>
+      <div className={styles.hero__wrapper}>
+        <section className={styles.hero}>
+          <h1 className={styles.landing__title}>A place where to hone my skills</h1>
+          <p>
+            See how I approach and solve Frontend coding problems. This site is a working progress
+            to learn React, testing and architecture matters.
+          </p>
+        </section>
+        <section className={styles.form__wrapper}>
+          <aside className={styles.disclaimer}>
+            <p>Login (local storage) to explore.</p>
+          </aside>
+          <article className={styles.form}>
+            <LoginForm />
+          </article>
+        </section>
+      </div>
     </div>
   );
 };
