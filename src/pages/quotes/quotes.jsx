aa4e392ddd69def55d55361b8f3e5df195d73ce1
Mercedes Egido <mercedes.egido@hey.com>
2024-10-28 16:22:42 +0100

    adding concepts to the quote

--- src/pages/quotes/quotes.jsx
+++ src/pages/quotes/quotes.jsx
@@ -7,31 +7,41 @@ const Quotes = () => {
 
   return (
     <section className={styles.wrapper}>
+      <section>
+        <p>Add quote</p>
+      </section>
       {quotes.map((quote) => (
         <section key={quote.author} className={styles.quote__wrapper}>
           <article className={styles.info}>
-            <div style={styles.title}>
+            <div className={styles.title}>
               <h2>{quote.author}</h2>
-            </div>
-            <p>{quote.fullText}</p>
-            <a href={quote.link} target="_blank" rel="noopener noreferrer">
-              Source link
-            </a>
-          </article>
-          <section className={styles.sides}>
-            <article>
-              {quote.tags.length !== 0 ? <h3>Tags</h3> : ''}
-              <div className={styles.side__items}>
+              <div className={styles.tags}>
                 {quote.tags.map((tag) => (
                   <p key={tag}>{`#${tag}`}</p>
                 ))}
               </div>
-            </article>
+            </div>
+            <p>{quote.fullText}</p>
+
+            <a href={quote.link} target="_blank" rel="noopener noreferrer">
+              Source link
+            </a>
+          </article>
+
+          <section className={styles.sides}>
             <article>
               {quote.references.length !== 0 ? <h3>References</h3> : ''}
               <div className={styles.side__items}>
                 {quote.references.map((reference) => (
                   <p key={reference}>{reference}</p>
+                ))}
+              </div>
+            </article>
+            <article>
+              {quote.concepts.length !== 0 ? <h3>Concepts</h3> : ''}
+              <div className={styles.side__items}>
+                {quote.concepts.map((concept) => (
+                  <p key={concept}>{concept}</p>
                 ))}
               </div>
             </article>
